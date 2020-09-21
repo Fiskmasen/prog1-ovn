77f0d9857443bd0537bd046dba0f1356665dd46d
Fiskmasen <70147767+Fiskmasen@users.noreply.github.com>
2020-09-21 14:59:27 +0200

    lektion måndag 21/9/2020

--- kvad.py
+++ kvad.py
@@ -2,3 +2,8 @@ svar = input ("Skriv ett heltal: ")
 x = int (svar)
 y = x * x
 print ("Talet i kvadrat är", y)
+
+svar = input ("Skriv ett heltal: ")
+x = float (svar)
+y = x * x
+print (f"Talet i kvadrat är {y:6.2f}")
